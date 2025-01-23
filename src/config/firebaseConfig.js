b1f115c6a7efa942b6032ea53eb65573c098350a
Mariano Macias <mariano.maciasgandulfo@gmail.com>
2025-01-23 09:02:59 +0100

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -7,13 +7,7 @@ import { getFirestore } from "firebase/firestore/lite";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDg66VmAvCe_NKQ8ZnBz4FLHULvSuDtW7M",
-  authDomain: "project-1701925568833932037.firebaseapp.com",
-  projectId: "project-1701925568833932037",
-  storageBucket: "project-1701925568833932037.firebasestorage.app",
-  messagingSenderId: "905934017401",
-  appId: "1:905934017401:web:3030be81906440c6f555a8",
-  measurementId: "G-DQT39WH1HZ"
+  // Tu credenciales AQUI
 };
 
 // Initialize Firebase
